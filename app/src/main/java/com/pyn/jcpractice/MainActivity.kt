498637bfc44bb4d6dadf3a87f7cbf004c9fa5893
visiongem <393507488@qq.com>
2021-03-24 13:35:28 +0800

    学习了： 1. 定义可组合函数 2. 使用 Column 并设置 Column 的样式，以改善布局 3. 根据 Material Design 原则设置应用的样式

--- app/src/main/java/com/pyn/jcpractice/MainActivity.kt
+++ app/src/main/java/com/pyn/jcpractice/MainActivity.kt
@@ -2,24 +2,61 @@ package com.pyn.jcpractice
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
 import androidx.ui.tooling.preview.Preview
-import com.pyn.jcpractice.ui.JCPracticeTheme
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JCPracticeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+            Greeting("Android")
+            NewsStory()
+        }
+    }
+}
+
+@Composable
+fun NewsStory() {
+    MaterialTheme() {
+        val typography = MaterialTheme.typography
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Image(
+                imageResource(id = R.drawable.header),
+                modifier = Modifier
+                    .height(180.dp)
+                    .fillMaxWidth()
+                    .clip(shape = RoundedCornerShape(6.dp)),
+                contentScale = ContentScale.Crop
+            )
+            Spacer(Modifier.preferredHeight(16.dp))
+            Text(
+                "A day wandering through the sandhills in Shark Fin Cove, and a few of the sights I saw",
+                style = typography.h6,
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis
+            )
+            Text(
+                "Davenport, California",
+                style = typography.body2
+            )
+            Text(
+                "December 2018",
+                style = typography.body1
+            )
         }
     }
 }
@@ -29,10 +66,14 @@ fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
 fun DefaultPreview() {
-    JCPracticeTheme {
-        Greeting("Android")
-    }
+    NewsStory()
+}
+
+@Preview
+@Composable
+fun PreviewGreeting() {
+    Greeting("Android")
 }
